ff91b9bd0f748d607ae043efc8b87061fb04ef46
randy1200 <randy1200@yahoo.com>
2016-07-10 08:56:53 -0700

    Uploaded formatting

--- index.php
+++ index.php
@@ -14,6 +14,36 @@
 </head>
 <body>
 	<img id="logo" src="logo_blue.png" alt="logo.png"/>
-	<h1><?php echo "Hello, World!!"; ?></h1>
+
+	<?php
+
+	$hamburgerPrice = 4.95;
+	$milkshakePrice = 1.95;
+	$colaPrice = .85;
+	$tax = .075;
+	$tip = .016;
+
+	printf('Prices\n');
+	print '';
+	printf('$4.2f : Hamburger\n', $hamburgerPrice);
+	printf('$4.2f : Milk Shake \n', $milkshakePrice);
+	printf('$4.2f : Cola \n', $colaPrice);
+	printf('$4.2f : Tax\n', $tax);
+	printf('$4.2f : Tip\n', $tip);
+	printf('');
+
+	$totalFood = $hamburgerPrice + $hamburgerPrice + $milkshakePrice + $colaPrice;
+	$pretaxTip = $totalFood * $tax;
+	$taxOnFood = $totalFood * $tax;
+
+	printf('Lunch Tab\n');
+	printf('$4.2f : Food Total\n', $totalFood);
+	printf('$4.2f : Pretax Tip\n', $pretaxTip);
+	printf('$4.2f : Tax On Food\n', $taxOnFood);
+	printf('$4.2f : Total\n', $totalFood + $pretaxTip + $taxOnFood);
+
+
+	?>
+
 </body>
 </html>
